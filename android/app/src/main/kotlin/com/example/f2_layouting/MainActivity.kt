17c43dc6196c81678beac2dbfa5539e2106e911d
ninedragonlabs <ninedragonlabs@gmail.com>
2023-03-20 19:14:30 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/f2_layouting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f2_layouting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Column"),
+        ),
+        // ================== Column ====================
+        // body: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     Container(
+        //       width: 200,
+        //       height: 50,
+        //       color: Colors.green,
+        //     ),
+        //     Container(
+        //       width: 50,
+        //       height: 50,
+        //       color: Colors.orange,
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 50,
+        //       color: Colors.amber,
+        //     ),
+        //     Container(
+        //       width: 300,
+        //       height: 50,
+        //       color: Colors.red,
+        //     ),
+        //   ],
+        // ),
+        // ================== Row ====================
+        // body: Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     Container(
+        //       height: 200,
+        //       width: 50,
+        //       color: Colors.green,
+        //     ),
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       color: Colors.orange,
+        //     ),
+        //     Container(
+        //       height: 100,
+        //       width: 50,
+        //       color: Colors.amber,
+        //     ),
+        //     Container(
+        //       height: 300,
+        //       width: 50,
+        //       color: Colors.red,
+        //     ),
+        //   ],
+        // ),
+        // ================== Stack ====================
+        body: Stack(
+          children: [
+            Container(
+              width: 400,
+              height: 400,
+              color: Colors.green,
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.orange,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
